764d0337477f0069c61bde95cd5d8094d2c9a9eb
Christian Baumann <33223146+chbauman@users.noreply.github.com>
2019-08-22 11:30:22 +0200

    Changed DataStruct Definition

--- BatchRL/data.py
+++ BatchRL/data.py
@@ -10,33 +10,25 @@ import restclient
 class DataStruct:
     """
     Base Class for different sets of data columns
-    defined by successive IDs
+    defined by successive IDs.
     """
     def __init__(self, 
-                 data_desc, 
-                 base_id, 
+                 id_list, 
                  name, 
                  startDate='2019-01-01',
                  endDate='2019-12-31'):
 
         # Initialize values and client
-        self.data_desc = data_desc
         self.base_id = base_id
         self.name = name
         self.startDate = startDate
         self.endDate = endDate
 
-        # Check if base_id is a list of ints or just an int,
-        # in that case create the list with the subsequent ints.
-        self.num_cols = len(data_desc)
-        try:
-            for ct, el in enumerate(base_id):
-                base_id[ct] = str(el)
-            self.data_ids = base_id
-        except TypeError as te:
-            self.data_ids = [str(base_id + i) for i in range(self.num_cols)]
+        # Convert elements of id_list to strings.
+        for ct, el in enumerate(id_list):
+            id_list[ct] = str(el)
+        self.data_ids = id_list
         self.client_loaded = False
-
         pass
 
     def load_client(self):
@@ -76,11 +68,7 @@ class DataStruct:
 # Test data, small part of the electricity data,
 # for testing since access is faster.
 TestData = DataStruct(
-            data_desc = [
-                "alarm active",
-                "th. power total",
-            ],
-            base_id = 421100171,
+            base_id = [421100171, 421100172],
             name = "Test",
             startDate='2019-08-08',
             endDate='2019-08-09'
@@ -88,27 +76,7 @@ TestData = DataStruct(
 
 # Electricity consumption data
 ElData = DataStruct(
-            data_desc = [
-                "alarm active",
-                "el. active power total",
-                "el. active power consumpted",
-                "el. active power L1",
-                "el. reactive power L1",
-                "el. cos Phi L1",
-                "el. current L1",
-                "el. voltage L1",
-                "el. active power L2",
-                "el. reactive power L2",
-                "el. cos Phi L2",
-                "el. current L2",
-                "el. voltage L2",
-                "el. active power L3",
-                "el. reactive power L3",
-                "el. cos Phi L3",
-                "el. current L3",
-                "el. voltage L3",
-            ],
-            base_id = 42190138,
+            base_id = [42190138 + i for in range(18)],
             name = "Electric",
             startDate='2019-01-01',
             endDate='2019-12-31'
@@ -116,49 +84,14 @@ ElData = DataStruct(
 
 # Heating data
 HeatingData = DataStruct(
-            data_desc = [
-                "alarm active",
-                "th. power total",
-                "th. energy total heating",
-                "volume flow",
-                "temperature high",
-                "temperature low",
-            ],
-            base_id = 421100171,
+            base_id = [421100171 + i for i in range(6)],
             name = "Heat-U33M1-P890",
             startDate='2019-01-01',
             endDate='2019-12-31'
             )
 
 # Valve data
-ValveData = DataStruct(data_desc=[
-                            "research mode approval",
-                            "research mode confirmation",
-                            "research mode status",
-                            "alarm active",
-                            "position relative",
-                            "research mode approval",
-                            "research mode confirmation",
-                            "research mode status",
-                            "alarm active",
-                            "position relative",
-                            "research mode approval",
-                            "research mode confirmation",
-                            "research mode status",
-                            "alarm active",
-                            "position relative",
-                            "research mode approval",
-                            "research mode confirmation",
-                            "research mode status",
-                            "alarm active",
-                            "position relative",
-                            "research mode approval",
-                            "research mode confirmation",
-                            "research mode status",
-                            "alarm active",
-                            "position relative",
-                            ],
-                       base_id=[421100138 + i for i in range(5)] \
+ValveData = DataStruct(base_id=[421100138 + i for i in range(5)] \
                              + [421100194 + i for i in range(5)] \
                              + [421100143 + i for i in range(5)] \
                              + [421100199 + i for i in range(5)] \
@@ -166,6 +99,6 @@ ValveData = DataStruct(data_desc=[
                        name="Valve-U33M1,U33N1-Y720,Y721,Y750",
                        startDate='2019-01-01',
                        endDate='2019-12-31'    
-    )
+                       )
 
 
