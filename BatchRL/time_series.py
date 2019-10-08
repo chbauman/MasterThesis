3347a3e70cad70fff50bfd1f54b22b75b4c4fead
Christian Baumann <chbauman@student.ethz.ch>
2019-10-08 17:42:41 +0200

    Pep8ed time-series

--- BatchRL/time_series.py
+++ BatchRL/time_series.py
@@ -1,26 +1,27 @@
-
 import numpy as np
 
 from statsmodels.tsa.arima_model import ARIMA
 
-# AR Time Series Model
-class AR_Model(object):
 
-    def __init__(self, lag = 1):
+class AR_Model(object):
+    """
+    AR Time Series Model.
+    """
+
+    def __init__(self, lag=1):
         self.lag = lag
 
-    def fit(self, data):
+        self.model = None
+        self.model_fit = None
 
+    def fit(self, data):
         self.model = ARIMA(data, order=(self.lag, 0, 0))
         self.model_fit = self.model.fit(trend='nc', disp=False)
 
     def predict(self, data):
-
-        ar_coef = self.model_fit.arparams
-        sig = sqrt(self.model_fit.sigma2)
-        yhat = 0.0
-        for i in range(1, len(ar_coef) + 1):
-            yhat += ar_coef[i-1] * data[-i]
-        return yhat + np.random.normal(0, sig)
-    pass
-
+        ar_coeff = self.model_fit.arparams
+        sig = np.sqrt(self.model_fit.sigma2)
+        y_hat = 0.0
+        for i in range(1, len(ar_coeff) + 1):
+            y_hat += ar_coeff[i - 1] * data[-i]
+        return y_hat + np.random.normal(0, sig)
