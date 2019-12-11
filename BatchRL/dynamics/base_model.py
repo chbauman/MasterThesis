Fixed scaling problems

BatchRL/dynamics/base_model.py
@@ -59,6 +59,36 @@ def _get_inds_str(indices: np.ndarray, pre: str = "In") -> str:
     return inds_str
 
 
+def check_model_compatibility(model_list: List['BaseDynamicsModel'], raise_error: bool = True) -> bool:
+    """Checks if models in list are compatible.
+
+    I.e. they need to predict the same series.
+
+    Args:
+        model_list: List with models.
+        raise_error: Whether to raise an error instead of returning a bool.
+
+    Returns:
+        Bool indicating whether models are compatible.
+
+    Raises:
+        ValueError: If models are not compatible and `raise_error` is True
+        AssertionError: If the `model_list` has length 0.
+    """
+    n_mods = len(model_list)
+    assert n_mods > 0, "No models provided!"
+    inds = model_list[0].out_inds
+    compat = True
+    for m in model_list:
+        curr_inds = m.out_inds
+        if not np.array_equal(inds, curr_inds):
+            compat = False
+            if raise_error:
+                raise ValueError(f"Indices {curr_inds} of model {m} not compatible "
+                                 f"with those of first model: {inds}")
+    return compat
+
+
 class BaseDynamicsModel(KerasBase, ABC):
     """This class describes the interface of a ML-based (partial) dynamics model.
     """
@@ -818,7 +848,7 @@ class BaseDynamicsModel(KerasBase, ABC):
 
 def compare_models(model_list: List[BaseDynamicsModel],
                    save_name: str, *,
-                   n_steps: Tuple[int, ...] = (1, ),
+                   n_steps: Tuple[int, ...] = (1,),
                    part_spec: str = "val",
                    model_names: List[str] = None) -> None:
     """Compares all the models in the list visually.

BatchRL/tests/test_dynamics.py
@@ -19,10 +19,10 @@ from tests.test_data import get_full_model_dataset, construct_test_ds
 from tests.test_util import TEST_DIR
 from util.numerics import copy_arr_list, MSE, MAE
 from util.util import Num, tot_size
+from util.visualize import plot_performance_graph
 
 
 def get_full_composite_model(standardized: bool = False) -> BaseDynamicsModel:
-
     # Get full dataset
     n = 100
     ds = get_full_model_dataset(n)
@@ -241,9 +241,33 @@ class TestBaseDynamics(TestCase):
                             "Streak data was changed during analysis!")
 
     def test_analyze_performance(self):
-        self.test_model_2.analyze_performance(n_steps=(1, 3), overwrite=True, verbose=0, n_days=7)
         met_list = (MSE, MAE)
-        self.test_model_2.analyze_performance(n_steps=(1, 3), overwrite=True, verbose=0, metrics=met_list, n_days=7)
+        self.test_model_2.analyze_performance(n_steps=(1, 3), overwrite=True,
+                                              verbose=0, metrics=met_list, n_days=7)
+
+    def test_performance_plot(self):
+        met_list = (MSE, MAE)
+        parts = ["Val", "Train"]
+        self.test_mod.analyze_performance(n_steps=(1, 3), overwrite=True,
+                                          verbose=0, metrics=met_list, n_days=7)
+        test_model_4 = ConstSeriesTestModel(self.ds, pred_val_list=[0.0, 2.0, 3.0])
+        test_model_4.analyze_performance(n_steps=(1, 3), overwrite=True,
+                                         verbose=0, metrics=met_list, n_days=7)
+        plot_name = "TestEvalPlot"
+        mods = [test_model_4, self.test_mod]
+        plot_performance_graph(mods, parts, met_list, plot_name,
+                               short_mod_names=["Test4", "Test2"],
+                               series_mask=np.array([0]),
+                               scale_back=True,
+                               remove_units=False,
+                               put_on_ol=False)
+        plot_performance_graph(mods, parts, met_list, plot_name,
+                               short_mod_names=["Test4", "Test2"],
+                               series_mask=np.array([0]),
+                               scale_back=True,
+                               remove_units=False,
+                               put_on_ol=False,
+                               compare_models=True)
 
     def test_const_series_test_model(self):
         # Test the test model

BatchRL/util/visualize.py
@@ -879,7 +879,7 @@ def _load_all_model_data(model_list: List,
         if ct == 0:
             inds = inds_curr
         else:
-            assert np.array_equal(inds, inds_curr)
+            assert np.array_equal(inds, inds_curr), f"Wrong indices: {inds_curr} != {inds}"
             check_shape(data_list[0], data.shape)
         data_list += [data]
 
@@ -1010,15 +1010,26 @@ def plot_performance_graph(model_list: List, parts: List[str],
                            remove_units: bool = True,
                            series_mask=None,
                            scale_back: bool = False,
-                           put_on_ol: bool = False) -> None:
+                           put_on_ol: bool = False,
+                           compare_models: bool = False) -> None:
     metric_names = [m.name for m in metric_list]
 
+    # Check if models are compatible
+    # check_model_compatibility(model_list)
+    if not np.any(model_list[0].data.is_scaled):
+        scale_back = False
+
     # Prepare the labels
-    series_descs, mod_names, scaling = _get_descs(model_list, remove_units, series_mask, short_mod_names)
+    series_descs, mod_names, scaling = _get_descs(model_list, remove_units,
+                                                  series_mask, short_mod_names)
 
     # Load data
     data_array, inds = _load_all_model_data(model_list, parts, metric_names, series_mask)
     n_models, n_parts, n_series, n_metrics, n_steps = data_array.shape
+
+    # Switch lists
+    if compare_models:
+        parts, mod_names = mod_names, parts
 
     plot_folder = OVERLEAF_IMG_DIR if put_on_ol else EVAL_MODEL_PLOT_DIR
     for model_ind, m_name in enumerate(mod_names):
@@ -1026,7 +1037,7 @@ def plot_performance_graph(model_list: List, parts: List[str],
         share_y = False
         dt = model_list[0].data.dt
 
-        if n_parts > len(joint_styles):
+        if len(parts) > len(joint_styles):
             warnings.warn("Duplicate plot styles!")
 
         ax1 = None
@@ -1047,14 +1058,17 @@ def plot_performance_graph(model_list: List, parts: List[str],
             plt.ylabel(m.name)
 
             # Plot all series
-            for set_id in range(n_parts):
+            for set_id, set_name in enumerate(parts):
                 for series_id in range(n_series):
                     # Get labels and errors
                     s_desc = series_descs[series_id]
                     if scale_back:
                         s_desc = _trf_desc_units(s_desc, m)
-                    lab = parts[set_id] + ": " + s_desc
-                    si = data_array[model_ind, set_id, series_id, ct_m]
+                    lab = set_name + ": " + s_desc
+                    if compare_models:
+                        si = data_array[set_id, model_ind, series_id, ct_m]
+                    else:
+                        si = data_array[model_ind, set_id, series_id, ct_m]
 
                     # Scale the errors
                     m_and_sd = scaling[series_id]
@@ -1068,7 +1082,7 @@ def plot_performance_graph(model_list: List, parts: List[str],
             # Add title, legend and x-label
             if ct_m == 0 or scale_back:
                 if ct_m == 0:
-                    plt.title(mod_names[model_ind])
+                    plt.title(m_name)
                 plt.legend()
             # if ct_m == len(metric_names) - 1:
             #     plt.xlabel(f"Steps [{mins_to_str(dt)}]")