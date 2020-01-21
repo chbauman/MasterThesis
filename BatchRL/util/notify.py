af3d7874f41de32af55ea276f410e6c066d99b30
Christian Baumann <chbauman@student.ethz.ch>
2020-01-21 14:55:16 +0100

    Added documentation and some error detection

--- BatchRL/util/notify.py
+++ BatchRL/util/notify.py
@@ -15,6 +15,7 @@ pw_def_path = os.path.join(curr_dir.parent.parent, "python_notifyer.txt")
 
 
 def _pw_from_file(file_name: str = pw_def_path):
+    """Reads the password in the file given."""
     with open(file_name, "r") as f:
         return f.read()
 
@@ -22,6 +23,17 @@ def _pw_from_file(file_name: str = pw_def_path):
 def send_mail(debug: bool = True,
               subject: str = "Hello there!",
               msg: str = "General Kenobi"):
+    """Sends a mail via python.
+
+    Args:
+        debug: Whether to use debug mode, will send the mail to the
+            debug address.
+        subject: Subject of the mail.
+        msg: Message of the mail.
+
+    Returns:
+
+    """
     rec_mail = debug_email if debug else receiver_email
 
     message = f"""Subject: {subject}\n\n\
@@ -31,4 +43,6 @@ def send_mail(debug: bool = True,
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
         server.login(sender_email, password)
-        server.sendmail(sender_email, rec_mail, message)
+        send_errs = server.sendmail(sender_email, rec_mail, message)
+        if len(send_errs) > 0:
+            print(f"Errors happened: {send_errs}")
