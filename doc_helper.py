845c980f59722a8514fb19e19dfae0dc0a137500
Christian Baumann <chbauman@student.ethz.ch>
2020-03-11 21:49:29 +0100

    Added more instructions

---
+++ doc_helper.py
@@ -0,0 +1,30 @@
+"""This is run before the documentation is built."""
+
+import os
+from shutil import copyfile
+
+
+DOC_DIR = "../MasterThesis/DocFiles"
+
+
+def main():
+    print("Setting up documentation...")
+    add_str = []
+    for f in os.listdir(DOC_DIR):
+        full_path = os.path.join(DOC_DIR, f)
+        a, _ = f.split(".")
+        add_str += [f"   {a}\n"]
+        dest_path = f"../Docs/{f}"
+        copyfile(full_path, dest_path)
+
+    s_ind = ":caption: Contents:"
+    with open("../Docs/index.rst", "r") as f:
+        data = f.read()
+        d1, d2 = data.split(s_ind)
+
+    with open("../Docs/index.rst", "w") as f:
+        f.write(d1 + s_ind + "\n\n" + "".join(add_str) + "   " + d2.lstrip())
+
+
+if __name__ == '__main__':
+    main()
--- make_doc.ps1
+++ make_doc.ps1
@@ -4,5 +4,6 @@ mkdir Docs
 cd Docs
 sphinx-apidoc -F -H 'BatchRL' -A 'Chris' -o . '../MasterThesis/BatchRL/'
 cp ../MasterThesis/conf.py .
+python ../MasterThesis/doc_helper.py
 ./make html SPHINXBUILD='python $(shell which sphinx-build)'
 cd ../MasterThesis
