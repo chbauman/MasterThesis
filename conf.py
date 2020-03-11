db819067e9c633c5a7229e72fab662dc3df134df
Christian Baumann <chbauman@student.ethz.ch>
2020-03-11 19:48:53 +0100

    Added relative path to conf.py

--- conf.py
+++ conf.py
@@ -13,7 +13,7 @@
 # import os
 import sys
 
-sys.path.insert(0, 'C:\\Users\\Chrigi\\Documents\\MasterThesis\\MasterThesis\\BatchRL')
+sys.path.insert(0, '..\\MasterThesis\\BatchRL')
 
 # -- Project information -----------------------------------------------------
 
